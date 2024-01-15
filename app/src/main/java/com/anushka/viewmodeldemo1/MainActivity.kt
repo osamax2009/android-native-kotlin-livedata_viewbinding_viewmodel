79bc0a882c519825bf38db65616c4179d8326ab7
osamax2009 <osamafalah85@yahoo.com>
2024-01-15 10:25:43 +0300

    simplecounter

--- app/src/main/java/com/anushka/viewmodeldemo1/MainActivity.kt
+++ app/src/main/java/com/anushka/viewmodeldemo1/MainActivity.kt
@@ -3,28 +3,44 @@ package com.anushka.viewmodeldemo1
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.anushka.viewmodeldemo1.databinding.ActivityMainBinding
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
        @SuppressLint("SetTextI18n")
-       lateinit var binding: ActivityMainBinding
-       lateinit var viewModel: MainActivityViewModel
+    var counter = 0
+    lateinit var textview : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
-        viewModel.count.observe(
-            this, {
-                binding.textView2.text = "count:$it"
-            }
-        )
+        setContentView(R.layout.activity_main)
+        // initialize our view
 
-        binding.button2.setOnClickListener(){
-            viewModel.update_counter()
+        textview = findViewById(R.id.tvCounter)
+        val  incrementButton = findViewById<Button>(R.id.incrementButton)
+        val  decrementButton  = findViewById<Button>(R.id.decrementButton)
+
+
+        incrementButton.setOnClickListener{
+            counter ++
+            updateCounterTextView()
         }
 
+        decrementButton.setOnClickListener{
+            counter --
+            updateCounterTextView()
+        }
+
+
+
+    }
+
+    fun updateCounterTextView (){
+        textview.text = counter.toString()
     }
 }
 
